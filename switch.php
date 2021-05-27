037c8e878ea9f1e0aa7b173baaab644448f330d6
Vims1012 <65559455+vimalgandhi@users.noreply.github.com>
2021-05-27 17:03:32 +0530

    Add files via upload

---
+++ switch.php
@@ -0,0 +1,29 @@
+
+<!-- switch case -->
+
+<!DOCTYPE html>
+<html>
+<body>
+
+
+       
+      <?php
+$a ='vimal';
+switch($a){
+ case ram:
+  echo "hello ram";
+  break;
+  case sam:
+  echo "hello sam";
+  break;
+  case vimal:
+  echo "hello vimal";
+  break;
+  default :
+  echo'plz selcet correct option';
+}
+?>
+
+
+</body>
+</html>
